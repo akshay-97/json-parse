c4a84dc212b4af944fd21486c6e26b8ab96df873
akshay.s <adiosphobian@gmail.com>
2025-03-03 19:33:07 +0530

    update bin

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Hello, World!"
+module P = Parler.Parser
+module Typ = Parler.Typ
+
+let () = 
+  match P.runParser P.stringJson "\"hello there\"" with
+  | Typ.Right (_json)  ->  print_endline "string parsed"
+  | Typ.Left err -> print_endline err 
